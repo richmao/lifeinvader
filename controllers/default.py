9810f20b9f30f9dcefe372ef4028002253d918b0
Richard Mao <rcmao@ucsc.edu>
2016-10-17 21:51:23 -0700

    overwrote template, new start. Kept databases made previously.

--- controllers/default.py
+++ controllers/default.py
@@ -1,75 +1,61 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# -------------------------------------------------------------------------
+# This is a sample controller
+# - index is the default action of any application
+# - user is required for authentication and authorization
+# - download is for downloading files uploaded in the db (does streaming)
+# -------------------------------------------------------------------------
+
+
 def index():
-    if auth.user: redirect(URL('home'))
-    return locals()
-# Testing git
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
 
 def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
     return dict(form=auth())
 
+
+@cache.action()
 def download():
-    return response.download(request,db)
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
 
 def call():
-    session.forget()
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
     return service()
 
-# our home page, will show our posts and posts by friends
-@auth.requires_login()
-def home():
-    Post.posted_by.default = me
-    Post.posted_on.default = request.now
-    crud.settings.formstyle = 'table2cols'
-    form = crud.create(Post)
-    friends = [me]+[row.target for row in myfriends.select(Link.target)]
-    posts = db(Post.posted_by.belongs(friends)).select(orderby=~Post.posted_on,limitby=(0,100))
-    return locals()
 
-# our wall, will show our profile and our own posts
-@auth.requires_login()
-def wall():
-    user = User(a0 or me)
-    if not user or not (user.id==me or myfriends(Link.target==user.id).count()):
-        redirect(URL('home'))
-    posts = db(Post.posted_by==user.id).select(orderby=~Post.posted_on,limitby=(0,100))
-    return locals()
-
-# a page for searching friends and requesting friendship
-@auth.requires_login()
-def search():
-    form = SQLFORM.factory(Field('name',requires=IS_NOT_EMPTY()))
-    if form.accepts(request):
-        tokens = form.vars.name.split()
-        query = reduce(lambda a,b:a&b,
-                       [User.first_name.contains(k)|User.last_name.contains(k) \
-                            for k in tokens])
-        people = db(query).select(orderby=alphabetical)
-    else:
-        people = []
-    return locals()
-
-# a page for accepting and denying friendship requests
-@auth.requires_login()
-def friends():
-    friends = db(User.id==Link.source)(Link.target==me).select(orderby=alphabetical)
-    requests = db(User.id==Link.target)(Link.source==me).select(orderby=alphabetical)
-    return locals()
-
-# this is the Ajax callback
-@auth.requires_login()
-def friendship():
-    """AJAX callback!"""
-    if request.env.request_method!='POST': raise HTTP(400)
-    if a0=='request' and not Link(source=a1,target=me):
-        # insert a new friendship request
-        Link.insert(source=me,target=a1)
-    elif a0=='accept':
-        # accept an existing friendship request
-        db(Link.target==me)(Link.source==a1).update(accepted=True)
-        if not db(Link.source==me)(Link.target==a1).count():
-            Link.insert(source=me,target=a1)
-    elif a0=='deny':
-        # deny an existing friendship request
-        db(Link.target==me)(Link.source==a1).delete()
-    elif a0=='remove':
-        # delete a previous friendship request
-        db(Link.source==me)(Link.target==a1).delete()
