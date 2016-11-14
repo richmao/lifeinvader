d1803695d57f8e094031bc68ab41187fe94285cd
Richard Mao <rcmao@ucsc.edu>
2016-11-14 11:23:12 -0800

    Liking and unliking

--- controllers/api.py
+++ controllers/api.py
@@ -4,7 +4,7 @@ import time
 def index():
     pass
 
-def get_posts():
+def get_images():
     """This controller is used to get the posts.  Follow what we did in lecture 10, to ensure
     that the first time, we get 4 posts max, and each time the "load more" button is pressed,
     we load at most 4 more posts."""
@@ -12,54 +12,41 @@ def get_posts():
     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
     end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
     # We just generate a lot of of data.
-    posts = []
+    images = []
     has_more = False
-    rows = db().select(db.post.ALL, orderby=~db.post.created_on, limitby=(start_idx, end_idx + 1))
+    rows = db().select(db.image.ALL, orderby=~db.image.created_on, limitby=(start_idx, end_idx + 1))
     for i, r in enumerate(rows):
         if i < end_idx - start_idx:
             t = dict(
                 id=r.id,
                 user_email=r.user_email,
-                post_content=r.post_content,
+                like_list=r.like_list,
                 created_on=r.created_on,
-                updated_on=r.updated_on,
             )
-            posts.append(t)
+            images.append(t)
         else:
             has_more = True
     logged_in = auth.user_id is not None
     return response.json(dict(
-        posts=posts,
+        images=images,
         logged_in=logged_in,
         has_more=has_more,
     ))
 
 
-# Note that we need the URL to be signed, as this changes the db.
-@auth.requires_signature()
-def add_post():
-    """Here you get a new post and add it.  Return what you want."""
-    p_id = db.post.insert(
-        #user_email=auth.user.email,
-        post_content=request.vars.post_content,
-        #created_on=datetime.datetime.utcnow(),
-        #updated_on=datetime.datetime.utcnow(),
+def unlike_image():
+    i = db(db.image.id == request.vars.image_id).select().first()
+    list = i.like_list
+    if request.vars.username in list: list.remove(request.vars.username)
+    i.update_record(
+        like_list=list,
     )
-    p = db.post(p_id)
-    return response.json(dict(post = p))
 
-
-@auth.requires_signature()
-def del_post():
-    """Used to delete a post."""
-    db(db.post.id == request.vars.post_id).delete()
-
-@auth.requires_signature()
-def edit_post():
-    """Used to edit a post."""
-    db.post(request.vars.post_id).update_record(
-        post_content=request.vars.post_content,
-        updated_on=datetime.datetime.utcnow(),
-        )
-    p = db.post(request.vars.post_id).updated_on;
-    return response.json(dict(updated_on=p))
+def like_image():
+    i = db(db.image.id == request.vars.image_id).select().first()
+    list = i.like_list
+    if request.vars.username not in list:
+        list = i.like_list + [request.vars.username]
+    i.update_record(
+        like_list=list,
+    )
--- static/js/default.js
+++ static/js/default.js
@@ -6,24 +6,24 @@ var app = function() {
 
     Vue.config.silent = false; // show all warnings
 
-    // Extends an array
+        // Extends an array
     self.extend = function(a, b) {
         for (var i = 0; i < b.length; i++) {
             a.push(b[i]);
         }
     };
 
-    function get_posts_url(start_idx, end_idx) {
+    function get_images_url(start_idx, end_idx) {
         var pp = {
             start_idx: start_idx,
             end_idx: end_idx
         };
-        return posts_url + "?" + $.param(pp);
+        return images_url + "?" + $.param(pp);
     }
 
-    self.get_posts = function () {
-        $.getJSON(get_posts_url(0, 4), function (data) {
-            self.vue.posts = data.posts;
+    self.get_images = function () {
+        $.getJSON(get_images_url(0, 4), function (data) {
+            self.vue.images = data.images;
             self.vue.has_more = data.has_more;
             self.vue.logged_in = data.logged_in;
         })
@@ -37,77 +37,24 @@ var app = function() {
         });
     };
 
-    self.add_post_button = function () {
-        // The button to add a post has been pressed.
-        self.vue.is_adding_post = !self.vue.is_adding_post;
-    };
-
-    self.edit_post_button = function (post_id) {
-        // The button to edit a post has been pressed.
-        self.vue.is_editing_post = true;
-        self.vue.editing_id = post_id;
-    };
-
-    self.add_post = function () {
-        // The submit button to add a post has been added.
-        $.post(add_post_url,
+    self.like_image = function(image_id)  {
+        $.post(like_image_url,
             {
-                post_content: self.vue.form_content,
-            },
-            function (data) {
-                $.web2py.enableElement($("#post-button"));
-                self.vue.posts.unshift(data.post);
-            });
-        self.vue.form_content = "";
-        //location.reload();
-    };
-
-    self.del_post = function (post_id) {
-        $.post(del_post_url,
-            {
-                post_id: post_id
+                image_id: image_id,
+                username: current_user
             },
             function () {
-                var idx = null;
-                for (var i = 0; i < self.vue.posts.length; i++) {
-                    if (self.vue.posts[i].id === post_id) {
-                        // If I set this to i, it won't work, as the if below will
-                        // return false for items in first position.
-                        idx = i + 1;
-                        break;
-                    }
-                }
-                if (idx) {
-                    self.vue.posts.splice(idx - 1, 1);
-                }
-            }
-        )
-    };
-
-    self.edit_post = function(post_id, edit_content)  {
-      $.post(edit_post_url,
-            {
-                post_id: post_id,
-                post_content: edit_content
-            },
-            function (data) {
-                $.web2py.enableElement($("#update-button"));
-                for(var i = 0; i < self.vue.posts.length; i++) {
-                    if(self.vue.posts[i].id == post_id) {
-                         self.vue.posts[i].updated_on = data.updated_on;
-                    }
-                }
             });
-        //location.reload();
     };
 
-    self.convert_time = function(pytime)    {
-            t = new Date(pytime);
-            return t.toLocaleString();
-    }
-
-    self.is_author = function(user) {
-        return current_user === user;
+    self.unlike_image = function(image_id)  {
+        $.post(unlike_image_url,
+            {
+                image_id: image_id,
+                username: current_user
+            },
+            function () {
+            });
     };
 
     // Complete as needed.
@@ -116,34 +63,37 @@ var app = function() {
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            is_adding_post: false,
-            is_editing_post: false,
-            posts: [],
             has_more: false,
             logged_in: false,
-            form_content: null,
-            editing_id: null,
         },
         methods: {
-            get_more: self.get_more,
-            add_post_button: self.add_post_button,
-            edit_post_button: self.edit_post_button,
-            add_post: self.add_post,
-            del_post: self.del_post,
-            edit_post: self.edit_post,
-            convert_time: self.convert_time,
-            is_author: self.is_author
+            like_image: self.like_image,
+            get_images: self.get_images,
+            unlike_image: self.unlike_image
         }
 
     });
 
-    self.get_posts();
+    //self.get_images();
     $("#vue-div").show();
 
 
     return self;
 };
 
+function toggleLike(id)
+{
+    console.log("toggle" + id);
+    var e = document.getElementById(id);
+    if (e.style.display == 'block' || e.style.display=='')
+    {
+        e.style.display = 'none';
+    }
+    else
+    {
+        e.style.display = 'block';
+    }
+}
 var APP = null;
 
 // This will make everything accessible from the js console;
