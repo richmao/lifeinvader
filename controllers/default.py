fbeb9b2dc65c5ca0e12c9ac3adaa791e417ce95d
tarballz <thenumber46853@gmail.com>
2016-11-30 13:33:24 -0800

    Cleared all dbs

--- controllers/default.py
+++ controllers/default.py
@@ -10,8 +10,7 @@ def index():
     comments = []
     if auth.user_id:
         followers = db(db.auth_user.username == auth.user.username).select(db.auth_user.follow_list).first().follow_list
-        images = db((db.image.author.belongs(followers)) | (db.image.author == auth.user.username)).select(db.image.ALL,
-                 orderby=~db.image.posted_on,limitby=(0, 20))
+        images = db((db.image.author.belongs(followers)) | (db.image.author == auth.user.username)).select(db.image.ALL, orderby=~db.image.posted_on,limitby=(0, 20))
 
         comments = db().select(db.image_comment.ALL)
     return dict(get_username_from_email=get_username_from_email, get_firstname_from_email=get_firstname_from_email,
